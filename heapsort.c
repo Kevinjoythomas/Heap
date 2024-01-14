f43192b00d03f204783fa75a94d2a975927edf33
Kevin Joy Thomas <113787279+Kevinjoythomas@users.noreply.github.com>
2024-01-14 18:28:53 +0530

    Update heapsort.c with comments

--- heapsort.c
+++ heapsort.c
@@ -1,79 +1,91 @@
 #include<stdio.h>
 #include<stdlib.h>
-int arr[20]={20,10,50,60,100,200,500,7,2,1000,75,2000,8000,1,300,275,90,350,10000,8};
 
-void minHeapify(int node,int n){
+// Global array to store heap elements
+int arr[20] = {20, 10, 50, 60, 100, 200, 500, 7, 2, 1000, 75, 2000, 8000, 1, 300, 275, 90, 350, 10000, 8};
 
-    int left=(2*node)+1;
-    int right=(2*node)+2;
-    int min=node;
-    if(left<n && arr[left]<arr[min]){
-        min=left;
+// Function to perform min-heapify operation on a node
+void minHeapify(int node, int n) {
+    int left = (2 * node) + 1;
+    int right = (2 * node) + 2;
+    int min = node;
+
+    if (left < n && arr[left] < arr[min]) {
+        min = left;
     }
-    if(right<n && arr[right]<arr[min]){
-        min=right;
+    if (right < n && arr[right] < arr[min]) {
+        min = right;
     }
 
-    if(node!=min){
-        int temp=arr[min];
-        arr[min]=arr[node];
-        arr[node]=temp;
-        minHeapify(min,n);
+    if (node != min) {
+        int temp = arr[min];
+        arr[min] = arr[node];
+        arr[node] = temp;
+        minHeapify(min, n);
+    }
+    return;
+}
+
+// Function to perform max-heapify operation on a node
+void maxHeapify(int node, int n) {
+    int left = (2 * node) + 1;
+    int right = (2 * node) + 2;
+    int max = node;
+
+    if (left < n && arr[left] > arr[max]) {
+        max = left;
+    }
+    if (right < n && arr[right] > arr[max]) {
+        max = right;
+    }
+
+    if (max != node) {
+        int temp = arr[max];
+        arr[max] = arr[node];
+        arr[node] = temp;
+        maxHeapify(max, n);
     }
     return;
 }
-void maxHeapify(int node,int n){
 
-    int left=(2*node)+1;
-    int right=(2*node)+2;
-    int max=node;
-    if(left<n && arr[left]>arr[max]){
-        max=left;
-    }
-    if(right<n && arr[right]>arr[max]){
-        max=right;
-    }
-    if(max!=node){
-        int temp=arr[max];
-        arr[max]=arr[node];
-        arr[node]=temp;
-        maxHeapify(max,n);
-    }
-    return;
-
+// Function to print the elements of the array
+void printTree(int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
 }
 
-void printTree(int size)
-{
-    for(int i=0;i<size;i++)
-    printf("%d ",arr[i]);
-}
-
-void heapsort(int n){
-    if(n==0){
+// Function to perform heap sort
+void heapsort(int n) {
+    if (n == 0) {
         return;
     }
 
-    int t=arr[n-1];
-    arr[n-1]=arr[0];
-    arr[0]=t;
-    maxHeapify(0,n-1);
-    heapsort(n-1);
+    int t = arr[n - 1];
+    arr[n - 1] = arr[0];
+    arr[0] = t;
+    maxHeapify(0, n - 1);
+    heapsort(n - 1);
 }
-int main(){
 
-    int n=20;
-    int lastnl=(n/2)-1;
+int main() {
+    int n = 20;
+    int lastnl = (n / 2) - 1;
 
-    for(int i=lastnl;i>=0;i--){
-        maxHeapify(i,n);
+    // Build max heap
+    for (int i = lastnl; i >= 0; i--) {
+        maxHeapify(i, n);
     }
 
-    printTree(20);
-    heapsort(20);
-    printf("\n");
+    // Print original array
+    printf("Original array:\n");
     printTree(20);
 
+    // Perform heap sort
+    heapsort(20);
+
+    // Print sorted array
+    printf("\nSorted array:\n");
+    printTree(20);
 
     return 0;
 }
